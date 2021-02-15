63a0b3eff1efb4fc45b1782160e5fc8f26cde523
Ryan Remaly <remalyryan@gmail.com>
2021-02-15 09:29:09 -0500

    Changing waka charts for mobile

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -5,7 +5,7 @@ import "./Projects.css";
 export default function Projects() {
   return (
     <>
-      <h1 className="projects">Projects</h1>
+      {/*<h1 className="projects">Projects</h1>*/}
       <div className="chartItem">
         <figure>
           <embed src="https://wakatime.com/share/@d6808870-d215-46d3-80bc-2d523b87089c/7ab4580a-61c3-42f2-8be6-b3d5d122ad72.svg"></embed>
